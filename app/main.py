8cf835e86c7eee6b714814046d37024d5313f252
ahmed magdy <ahamagdy@noon.com>
2024-12-18 14:57:47 +0200

    Respond to multiple PINGs

--- app/main.py
+++ app/main.py
@@ -1,22 +1,43 @@
 import socket
 
-def start_server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind(('localhost', 6379))  # Binding to localhost and port 6379
-    client_socket, client_address = server.accept()
-    while True:
-        data = client_socket.recv(1024)
-        if not data:
-            break
-        reply = f"PONG\r\n"
-        client_socket.send(reply.encode('utf-8'))
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_socket, client_address = server_socket.accept()
-    data = client_socket.recv(1024)
-    reply = f"+PONG\r\n"
-    client_socket.send(reply.encode('utf-8'))
+
+def create_socket_server():
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        server.bind(('localhost', 6379))
+
+        server.listen(1)
+
+        print("Server is listening on localhost:6379")
+
+        client_socket, client_address = server.accept()
+        print(f"Connection from {client_address}")
+
+        while True:
+            try:
+                data = client_socket.recv(1024)
+                if not data:
+                    break
+
+                reply = "+PONG\r\n"
+                for msg in data.splitlines():
+                    msg = msg.decode('utf-8')
+                    if msg == "PING":
+                        client_socket.send(reply.encode('utf-8'))
+
+            except ConnectionResetError:
+                print("Client disconnected")
+                break
+
+        # Close sockets
+        client_socket.close()
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
-    start_server()
+    create_socket_server()
