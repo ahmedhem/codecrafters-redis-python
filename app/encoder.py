4bbd05aa80cbf8ed6bb77e587d2d802960fa4a04
ahmed magdy <ahamagdy@noon.com>
2024-12-21 16:46:15 +0200

    add non array type

--- app/encoder.py
+++ app/encoder.py
@@ -3,13 +3,17 @@ from typing import List
 
 class Encoder:
     lines: List[str]
+    is_array: False
 
     def __init__(self, lines: List[str]):
         self.lines = lines
         pass
 
     def execute(self):
-        response = "*" + str(len(self.lines)) + "\r\n"
+        response = ""
+        if self.is_array:
+           response += "*" + str(len(self.lines)) + "\r\n"
+
         for word in self.lines:
             response += "$" + str(len(word)) + "\r\n" + word + "\r\n"
 
