61e6cc1eadb6b5a3caabb126999fa96a067a90f7
ahmed-magdy-noon <f.ahmadhem@gmail.com>
2025-02-01 11:47:02 +0200

    codecrafters submit [skip ci]

--- src/events/__init__.py
+++ src/events/__init__.py
@@ -1,2 +1,2 @@
-from . import ping, base, keys, set, get, wait, psync, info, echo, config, replconf, type, XADD, XRANGE, XREAD, INCR
+from . import ping, base, keys, set, get, wait, psync, info, echo, config, replconf, type, XADD, XRANGE, XREAD, INCR, MULTI
 from.XADD import REDIS_STREAM
